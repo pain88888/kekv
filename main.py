b77f3046d1a32c98a9a671b3d4e15b46604e311c
pain88888 <82121681+pain88888@users.noreply.github.com>
2021-05-17 03:54:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 def main():
     return "<h1>Welcome Home !</h1>"
 
-@app.route("/users/<name>/")
-def goto_user(name):
-    return f"<h1>Welcome {name}</h1>"
 
 
 if __name__ == "__main__":
